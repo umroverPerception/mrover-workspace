49c562679f2a6421bf347e43f8f9fcdb0678d410
JayeshM29 <jayeshsmate@gmail.com>
2021-03-07 14:07:57 -0500

    Created OccupancyMap class and Mapping Class

--- jetson/percep/mapping.cpp
+++ jetson/percep/mapping.cpp
@@ -1,34 +1,130 @@
-#include 'mapping.hpp'
+#include "mapping.hpp"
+#include "utilities.hpp"
+#include "rover_msgs/Odometry.hpp"
 
-#define DEFAULT_OCCUPANCY_MAP_HEIGHT 10000
-#define DEFAULT_OCCUPANCY_MAP_WIDTH 10000
+#include <iostream>
+#include <vector>
+#include <cmath>
 
 OccupancyMap::OccupancyMap() {
     occupancyMapHeight = DEFAULT_OCCUPANCY_MAP_HEIGHT;
     occupancyMapWidth = DEFAULT_OCCUPANCY_MAP_WIDTH;
-    fillOccupanyMap(occupancyMap, doubleToChar(0.5));
-}
-        
-char OccupancyMap::doubleToChar(double input) {
-    return ceil(input / CONVERSION_FACTOR);
-}
-        
-double OccupancyMap::charToDouble(char input) {
-    return input * CONVERSION_FACTOR;
+    occupancyMap.resize(occupancyMapHeight);
+    for (std::size_t i = 0; i < occupancyMap.size(); ++i) {
+        occupancyMap[i].resize(occupancyMapWidth);
+    }
 }
 
-int OccupancyMap::getHeightOfMap() {
-    return occupancyMap.size();
+OccupancyMap::OccupancyMap(int height, int width, char charToFill) : occupancyMapHeight(height), occupancyMapWidth(width), charToFill(static_cast<char>(128)) {
+    occupancyMap.resize(occupancyMapHeight);
+    for (std::size_t i = 0; i < occupancyMap.size(); ++i) {
+        occupancyMap[i].resize(occupancyMapWidth);
+    } 
 }
 
-int OccupancyMap::getWidthOfOccupancyMap() {
-    return occupancyMap.at(0).size();
-}
-
-void OccupancyMap::fillOccupanyMap(std::vector<std::vector<char> > &occupancyMap, char toFillWith) {
-    for(int row = 0; row < getHeightOfMap(); row++) {
-        for(int col = 0; col < getWidthOfOccupancyMap(row).size(); col++) {
-            occupancyMap.at(row).at(col).pushback(toFillWith);
+void OccupancyMap::fillOccupanyMap() {
+    for(std::size_t height = 0; width < occupancyMapHeight; height++) {
+        for(std::size_t width = 0; width < occupancyMapWidth; width++) {
+            occupancyMap[height][width] = charToFill;
         }
     }
 }
+
+double& OccupancyMap::operator()(int row, int col) {
+    return occupancyMap[row][col] * CONVERSION_FACTOR;
+}
+
+double& OccupancyMap::operator=(double &value) {
+    return ceil(value / CONVERSION_FACTOR);
+}
+
+Mapping::Mapping() {
+    //defaultCellDistanceRepresentation
+    cellDistance = 0.3; //0.3 meters
+    occupancyMap = new OccupancyMap();
+    previousOdometry = new Odometry();
+    roverXCoordsInOccupancyMap = 4999;
+    roverXCoordsInOccupancyMap = 4999;
+}
+
+void Mapping::updatePositionInOccupancyMap(Odometry &currentOdometry) {
+    double changeInDistanceFromPreviousPosition = 
+            estimateNoneuclid(currentOdometry, previousOdometry);
+        double changeInBearingFromPreviousPosition = 
+            calcBearing(currentOdometry, previousOdometry);
+        
+        while(changeInBearingFromPreviousPosition < 0) {
+            changeInBearingFromPreviousPosition += 360.0;
+        }
+
+        //if the bearing is between 0 and 90
+        if(changeInBearingFromPreviousPosition =< 90.0) {
+            changeInPositionWidth = changeInEuclideanDistanceFromPreviousPosition * 
+                cos(90.0 - changeInBearingFromPreviousPosition);
+            changeInPositionHeight = changeInEuclideanDistanceFromPreviousPosition * 
+                sin(90.0 - changeInBearingFromPreviousPosition);
+        }
+        //if bearing is between 90 - 180 
+        else if(changeInBearingFromPreviousPosition > 90.0 && changeInBearingFromPreviousPosition =< 180.0) {
+            changeInPositionWidth = cos(changeInBearingFromPreviousPosition - 90.0);
+            changeInPositionHeight = sin(changeInBearingFromPreviousPosition - 90.0);
+        }
+        else if(changeInBearingFromPreviousPosition > 180.0 && changeInBearingFromPreviousPosition =< 270.0) {
+            changeInPositionWidth = cos(270.0 - changeInBearingFromPreviousPosition); 
+            changeInPositionHeight = sin(270.0 - changeInBearingFromPreviousPosition);               
+        }
+        else {
+            changeInPositionWidth = cos(changeInBearingFromPreviousPosition - 270.0);
+            changeInPositionHeight = sin(hangeInBearingFromPreviousPosition - 270.0);
+        }
+
+        roverXCoordsInOccupancyMap += ceil(changeInPositionWidth/cellDistance);
+        roverYCoordsInOccupancyMap += ceil(changeInPositionHeight/cellDistance);
+        
+        //then update odometry
+        previousOdometry = currentOdometry;
+}
+
+/*char OccupancyMap::doubleToChar(double &input) {
+    return ceil(input / CONVERSION_FACTOR);
+}
+        
+double OccupancyMap::charToDouble(char &input) {
+    return input * CONVERSION_FACTOR;
+}
+
+int OccupancyMap::getMapHeight() {
+    return occupancyMapHeight;
+}
+
+int OccupancyMap::getMapWidth() {
+    return occupancyMapWidth;
+}
+
+void OccupancyMap::fillOccupanyMap(char &toFillWith) {
+    for(std::size_t row = 0; row < occupancyMap.size(); ++row) {
+        for(std::size_t col = 0; col < occupancyMap[row].size(); ++col) {
+            occupancyMap[row][col] = toFillWith;
+        }
+    }
+}
+
+double OccupancyMap::convertDegreesToMeters(double degrees) {
+    return degrees * DEGREES_TO_METERS_CONVERSION_FACTOR;
+}
+double OccupancyMap::changeInLatitudeMeters(double currentLatitude) {
+    return convertDegreesToMeters(previousLatitude - currentLatitude);
+}
+
+double OccupancyMap::changeInLongitudeMeters(double currentLongitude); {
+    return convertDegreesToMeters(previousLongitude - currentLongitude);
+}
+
+void updatePosition() {
+    currentPositionInOccupancyMapHeight =+ round(changeInLatitudeMeters(currentLatitude));
+    currentPositionInOccupancyMapLength =+ round(changeInLongitudeMeters(currentLongitude));
+}
+
+int getCurrentPositionInOccupancyMapHeight() return currentPositionInOccupancyMapHeight;
+int getCurrentPositionInOccupancyMapLength() return currentPositionInOccupancyMapLength;
+*/
--- jetson/percep/mapping.hpp
+++ jetson/percep/mapping.hpp
@@ -1,15 +1,56 @@
-#pragma once
+#ifndef PERCEP_MAPPING
+#define PERCEP_MAPPING
 
 #include <vector>
-#include <cmath>
+#include "rover_msgs/Odometry.hpp"
+
+const std::size_t DEFAULT_OCCUPANCY_MAP_HEIGHT = 10000;
+const std::size_t DEFAULT_OCCUPANCY_MAP_WIDTH = 10000;
+const double CONVERSION_FACTOR = 0.004;
 
 class OccupancyMap {
+private:
+    std::vector<std::vector<char> > occupancyMap;
+    size_t occupancyMapHeight, occupancyMapWidth;
+    char charToFill;
+    
+    void fillOccupancyMap();
+public:
+    //default constructor for occupancy map
+    OccupancyMap();
+
+    //constructor with given length and width
+    OccupancyMap(int length, int width, char charToFill);
+
+    double &operator()(int height, int width);
+
+    double &operator=(double &value);
+}
+
+class Mapping {
+private:
+    OccupancyMap map;
+    Odometry previousOdometry;
+    int roverXCoordsInOccupancyMap, roverYCoordsInOccupancyMap;
+    double cellDistance;
+
+public:
+    Mapping();
+
+    void updatePositionInOccupancyMap(Odometry &currentOdometry);
+}
+
+/*class OccupancyMap {
     private:
         double CONVERSION_FACTOR = 0.004;
         const double INITIALIZE_VECTOR_PROBABILITY = 0.5;
         std::vector<std::vector<char> > occupancyMap;
         double occupancyMapHeight;
         double occupancyMapWidth;
+        double previousLatitude, previousLongitude;
+        const double DEGREES_TO_METERS_CONVERSION_FACTOR = 111317.0997;
+        double sizeOfOneCellInMeters = 0.3;
+        currentPositionInOccupancyMapHeight, currentPositionInOccupancyMapLength;
 
         
     public:
@@ -17,17 +58,40 @@ class OccupancyMap {
         OccupancyMap();
 
         //converts double to char
-        char doubleToChar(double input);
+        char doubleToChar(double &input);
         
         //converts char to double
-        double charToDouble(char input);
+        double charToDouble(char &input);
 
         //gets height of occupancyMap
-        int getHeightOfMap();
+        int getMapHeight();
 
         //gets width of occupancyMap
-        int getWidthOfOccupancyMap();
+        int getMapWidth();
 
         //fills occupancyMap with a given char "toFillWith"
-        void fillOccupanyMap(std::vector<std::vector<char> > &occupancyMap, char toFillWith);
-};
+        void fillOccupanyMap(char &toFillWith);
+
+        //converts deg min into degrees
+        double convertDegreesToMinutes(int &degreeMin);
+
+        //converts degrees to meteres
+        //based on NASA's diameter of earth 
+        //https://imagine.gsfc.nasa.gov/features/cosmic/earth_info.html
+        //and then math
+        const double convertDegreesToMeters(double degrees) {
+            return degrees * 111317.0997;
+        }
+
+        //gets the change in latitude in meters
+        double changeInLatitudeMeters(double currentLatitude);
+    
+
+        //gets the change in latitude in meters
+        double changeInLongitudeMeters(double currentLongitude);
+
+        //updates robot position
+        void updatePosition()
+
+};*/
+#endif
